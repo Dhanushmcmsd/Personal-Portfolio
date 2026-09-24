ce5671f47c72d4452b7357aa0e59e7e175d95828
Dhanushmcmsd <mc.dhanush.msd@gmail.com>
2026-09-24 22:17:45 +0530

    sitemapping

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+const baseUrl = "https://dhanush-portfolio-d3d.vercel.app";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
